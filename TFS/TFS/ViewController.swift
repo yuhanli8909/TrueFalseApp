a8c41e164c71120e120bb1ef38ede443fd8069ef
yuhanli8909 <yuhan0921@qq.com>
2017-04-22 15:09:45 +0800

    Initial commit

---
+++ TFS/TFS/ViewController.swift
@@ -0,0 +1,146 @@
+//
+//  ViewController.swift
+//  TFS
+//
+//  Created by Yuhan Li on 22/04/2017.
+//  Copyright © 2017 Yuhanli. All rights reserved.
+//
+
+import UIKit
+import GameKit
+import AudioToolbox
+
+class ViewController: UIViewController {
+    
+    let questionsPerRound = 4
+    var questionsAsked = 0
+    var correctQuestions = 0
+    
+    
+    var gameSound: SystemSoundID = 0
+    
+    
+    @IBOutlet weak var questionField: UILabel!
+    @IBOutlet weak var Option1: UIButton!
+    @IBOutlet weak var Option2: UIButton!
+    @IBOutlet weak var Option3: UIButton!
+    @IBOutlet weak var Option4: UIButton!
+    @IBOutlet weak var playAgainButton: UIButton!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        loadGameStartSound()
+        
+        // Start game
+        playGameStartSound()
+        displayQuestion()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    let triviaList = TriviaList()
+    
+    //method that shows the Questrion<string> in the questionField
+    func displayQuestion() {
+        
+        let trivia = triviaList.selectQuestion()
+        
+        questionField.text = trivia.Question
+        playAgainButton.isHidden = true
+        
+        Option1.setTitle(trivia.Options[0], for: UIControlState.normal)
+        Option1.tag = 0
+        
+        Option2.setTitle(trivia.Options[1], for: UIControlState.normal)
+        Option2.tag = 1
+        
+        Option3.setTitle(trivia.Options[2], for: UIControlState.normal)
+        Option3.tag = 2
+        
+        Option4.setTitle(trivia.Options[3], for: UIControlState.normal)
+        Option4.tag = 3
+        
+    }
+    
+    func displayScore() {
+        // Hide the answer buttons
+        Option1.isHidden = true
+        Option2.isHidden = true
+        Option3.isHidden = true
+        Option4.isHidden = true
+        
+        // Display play again button
+        playAgainButton.isHidden = false
+        
+        questionField.text = "Way to go!\nYou got \(correctQuestions) out of \(questionsPerRound) correct!"
+        
+    }
+    
+    @IBAction func checkAnswer(_ sender: UIButton) {
+        // Increment the questions asked counter
+        questionsAsked += 1
+        
+        if (sender.tag == triviaList.selectQuestion().correctAnswer) {
+            correctQuestions += 1
+            questionField.text = "Correct!"
+        } else {
+            questionField.text = "Sorry, wrong answer!"
+        }
+        
+        loadNextRoundWithDelay(seconds: 2)
+    }
+    
+    func nextRound() {
+        if questionsAsked == questionsPerRound {
+            // Game is over
+            displayScore()
+        } else {
+            // Continue game
+            displayQuestion()
+        }
+    }
+    
+    @IBAction func playAgain() {
+        // Show the answer buttons
+        Option1.isHidden = false
+        Option2.isHidden = false
+        Option3.isHidden = false
+        Option4.isHidden = false
+        
+        questionsAsked = 0
+        correctQuestions = 0
+        nextRound()
+    }
+    
+    
+    
+    // MARK: Helper Methods
+    
+    func loadNextRoundWithDelay(seconds: Int) {
+        // Converts a delay in seconds to nanoseconds as signed 64 bit integer
+        let delay = Int64(NSEC_PER_SEC * UInt64(seconds))
+        // Calculates a time value to execute the method given current time and delay
+        let dispatchTime = DispatchTime.now() + Double(delay) / Double(NSEC_PER_SEC)
+        
+        // Executes the nextRound method at the dispatch time on the main queue
+        DispatchQueue.main.asyncAfter(deadline: dispatchTime) {
+            self.nextRound()
+        }
+    }
+    
+    func loadGameStartSound() {
+        let pathToSoundFile = Bundle.main.path(forResource: "GameSound", ofType: "wav")
+        let soundURL = URL(fileURLWithPath: pathToSoundFile!)
+        AudioServicesCreateSystemSoundID(soundURL as CFURL, &gameSound)
+    }
+    
+    func playGameStartSound() {
+        AudioServicesPlaySystemSound(gameSound)
+    }
+}
+
+
